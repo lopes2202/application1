c238294d4827db2cfcda3b6b2b8da4346c45bb07
Gabriel Lopes <gabriel.ldbrito@gmail.com>
2025-05-16 16:34:52 -0300

    a

--- firebaseconfig.js
+++ firebaseconfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_AUTH_DOMAIN",
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_BUCKET",
-  messagingSenderId: "SEU_MESSAGING_SENDER_ID",
-  appId: "SEU_APP_ID"
+  apiKey: '',
+  authDomain: 'localhost/8000.firebaseapp.com',
+  projectId: 'application-1-bddac',
+  storageBucket: 'SEU_BUCKET',
+  messagingSenderId: 'SEU_SENDER_ID',
+  appId: 'SEU_APP_ID',
 };
 
 const app = initializeApp(firebaseConfig);
